6436475c6e14f32b094be224c3d433999783d497
EricSanchez <niexiaohangeric@163.com>
2024-04-09 23:17:04 +0800

    测试二

--- src/main/scene/role/player/Yao.gd
+++ src/main/scene/role/player/Yao.gd
@@ -3,8 +3,8 @@ extends Player
 
 func _ready() -> void:
 	weapons_track.register_weapon(1, weapons.instance_weapon('normal_dagger'))
-	weapons_track.register_weapon(2, weapons.instance_weapon('normal_sword'))
-	weapons_track.register_weapon(3, weapons.instance_weapon('normal_sword'))
+	weapons_track.register_weapon(2, weapons.instance_weapon('normal_dagger'))
+	weapons_track.register_weapon(3, weapons.instance_weapon('normal_dagger'))
 	weapons_track.register_weapon(4, weapons.instance_weapon('normal_sword'))
 	weapons_track.register_weapon(5, weapons.instance_weapon('normal_sword'))
 	weapons_track.register_weapon(6, weapons.instance_weapon('normal_dagger'))
