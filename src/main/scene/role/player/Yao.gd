d594272cc0256634761432e1c99ab6be4c4d42f4
鲍喜 <2523483565@qq.com>
2024-04-09 23:13:01 +0800

    武器改变

--- src/main/scene/role/player/Yao.gd
+++ src/main/scene/role/player/Yao.gd
@@ -2,7 +2,7 @@ extends Player
 
 
 func _ready() -> void:
-	weapons_track.register_weapon(1, weapons.instance_weapon('normal_sword'))
+	weapons_track.register_weapon(1, weapons.instance_weapon('normal_dagger'))
 	weapons_track.register_weapon(2, weapons.instance_weapon('normal_sword'))
 	weapons_track.register_weapon(3, weapons.instance_weapon('normal_sword'))
 	weapons_track.register_weapon(4, weapons.instance_weapon('normal_sword'))
