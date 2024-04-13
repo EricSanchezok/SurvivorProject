c0be27c62756df682ffdac52fbb6446f68b3b701
EricSanchez <niexiaohangeric@163.com>
2024-04-13 17:31:09 +0800

    同步

--- src/main/scene/role/player/Yao.gd
+++ src/main/scene/role/player/Yao.gd
@@ -2,14 +2,14 @@ extends Player
 
 
 func _ready() -> void:
-	register_weapon.emit(self, "normal_dagger", 1)
-	register_weapon.emit(self, "normal_dagger", 2)
-	register_weapon.emit(self, "normal_dagger", 3)
-	register_weapon.emit(self, "normal_dagger", 4)
-	register_weapon.emit(self, "normal_dagger", 5)
-	register_weapon.emit(self, "normal_dagger", 6)
-	register_weapon.emit(self, "normal_dagger", 7)
-	register_weapon.emit(self, "normal_dagger", 8)
-	register_weapon.emit(self, "normal_dagger", 9)
-	register_weapon.emit(self, "normal_dagger", 10)
+	register_weapon.emit(self, "normal_bow", 1)
+	register_weapon.emit(self, "normal_bow", 2)
+	register_weapon.emit(self, "normal_bow", 3)
+	register_weapon.emit(self, "normal_bow", 4)
+	register_weapon.emit(self, "normal_bow", 5)
+	register_weapon.emit(self, "normal_bow", 6)
+	register_weapon.emit(self, "normal_bow", 7)
+	register_weapon.emit(self, "normal_bow", 8)
+	register_weapon.emit(self, "normal_bow", 9)
+	register_weapon.emit(self, "normal_bow", 10)
 	pass
