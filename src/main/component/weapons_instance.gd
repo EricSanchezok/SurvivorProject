5e58587dec465975cb98b27d5622bffda06a6e5b
EricSanchez <niexiaohangeric@163.com>
2024-04-14 01:00:40 +0800

    究极同步

--- src/main/component/weapons_instance.gd
+++ src/main/component/weapons_instance.gd
@@ -7,6 +7,9 @@ extends Node
 	"normal_shield": preload("res://src/main/scene/role/weapons/Shield/normal_shield.tscn"),
 	"normal_dagger": preload("res://src/main/scene/role/weapons/Dagger/normal_dagger.tscn"),
 	"normal_bow": preload("res://src/main/scene/role/weapons/Bow/normal_bow.tscn"),
+	"normal_axe": preload("res://src/main/scene/role/weapons/Axe/normal_axe.tscn"),
+	"normal_spear": preload("res://src/main/scene/role/weapons/Spear/normal_spear.tscn"),
+	"normal_boomerang": preload("res://src/main/scene/role/weapons/Boomerang/normal_boomerang.tscn"),
 }
 
 
