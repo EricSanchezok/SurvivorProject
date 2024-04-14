ac340519718b02ba87a5bd6c07847408d858ea03
EricSanchez <niexiaohangeric@163.com>
2024-04-15 00:46:06 +0800

    415同步

---
+++ .history/src/main/scene/ui/pause_screen_20240414223521.gd
@@ -0,0 +1,78 @@
+extends Control
+
+
+var viewport = get_viewport()
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 状态机 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+enum State {
+	IDLE,
+	INVENTORY,
+	COMBAT_PROGRESS,
+	EQUIPMENT,
+	QUESTS,
+	SAVE_LOAD,
+	SETTINGS
+}
+
+
+func tick_physics(state: State, delta: float) -> void:
+    # 打印视口中心
+    print(viewport.get_camera_position())
+
+	match state:
+		State.IDLE:
+			pass
+		State.INVENTORY:
+			pass
+		State.COMBAT_PROGRESS:
+			pass
+		State.EQUIPMENT:
+			pass
+		State.QUESTS:
+			pass
+		State.SAVE_LOAD:
+			pass
+		State.SETTINGS:
+			pass
+			
+func get_next_state(state: State) -> int:
+	match state:
+		State.IDLE:
+			pass
+		State.INVENTORY:
+			pass
+		State.COMBAT_PROGRESS:
+			pass
+		State.EQUIPMENT:
+			pass
+		State.QUESTS:
+			pass
+		State.SAVE_LOAD:
+			pass
+		State.SETTINGS:
+			pass
+
+	return StateMachine.KEEP_CURRENT
+	
+func transition_state(from: State, to: State) -> void:	
+	# print("[%s] %s => %s" % [
+	# 	Engine.get_physics_frames()	,
+	# 	State.keys()[from] if from != -1 else "<START>",
+	# 	State.keys()[to],
+	# ])
+	
+	match to:
+		State.IDLE:
+			pass
+		State.INVENTORY:
+			pass
+		State.COMBAT_PROGRESS:
+			pass
+		State.EQUIPMENT:
+			pass
+		State.QUESTS:
+			pass
+		State.SAVE_LOAD:
+			pass
+		State.SETTINGS:
+			pass
